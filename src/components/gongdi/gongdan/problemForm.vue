16b81aed9cba0baf1779d3ad4a51486ed4e15363
yangyipeng888 <506977655@qq.com>
2020-12-15 17:52:07 +0800

    20201215

---
+++ src/components/gongdi/gongdan/problemForm.vue
@@ -0,0 +1,193 @@
+<template>
+  <div class="qualityForm1">
+    <form1>
+      <template v-slot:title>
+        {{title}}
+      </template>
+      <template v-slot:list-title>
+        <div class="listTitle">
+          <div class="title_time">时间</div>
+          <div class="title_type">类型</div>
+          <div class="title_status">状态</div>
+          <div class="title_opera">操作</div>
+        </div>
+      </template>
+      <template v-slot:list>
+        <div class="problem_list" v-show="gongdanList&&gongdanList.length"
+             v-for="pro,index in gongdanList" :key="index">
+          <div class="problem_time van-ellipsis">{{showTime(pro.work.createTime)}}</div>
+          <div class="problem_type van-ellipsis">{{pro.work.nodeType}}</div>
+          <div class="problem_status">
+            <div class="status_txt" :class="bindStateClass(pro.work.nodeState)">
+              {{showNodeState(pro.work.nodeState)}}
+            </div>
+          </div>
+          <div @click="clickMore(pro)" class="problem_opera">查看</div>
+
+        </div>
+      </template>
+    </form1>
+  </div>
+
+</template>
+
+<script>
+  import form1 from '@/components/gongdi/common/form'
+
+  export default {
+    name: 'problemForm',
+    props: {
+      title: {
+        type: String,
+        default: '工单'
+      },
+      gongdanList: {
+        type: Array,
+        default: []
+      }
+    },
+    components: {
+      form1
+    },
+    mounted() {
+    },
+    methods: {
+      bindStateClass(nodeState) {
+        switch (nodeState) {
+          case this.myConst.GD_NODE_STATE.NOT:
+            return { status_nok: true }
+          case this.myConst.GD_NODE_STATE.WAITING:
+            return { status_appoint: true }
+          default:
+            return { status_ok: true }
+
+        }
+      },
+      showTime(time) {
+        return new Date(time).toLocaleDateString().split(' ')[0]
+      },
+      showNodeState(nodeState) {
+        return this.myConst.GD_NODE_STATE_LABEL[nodeState]
+      },
+      clickMore(pro) {
+
+      }
+    },
+    data() {
+      return {}
+
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .qualityForm1 {
+
+    .listTitle {
+      display: flex;
+      flex-direction: row;
+      text-align: center;
+      height: 30px;
+      line-height: 30px;
+
+      .title_time {
+        width: 30%;
+      }
+
+      .title_type {
+        width: 25%;
+      }
+
+      .title_status {
+        width: 25%;
+
+      }
+
+      .title_opera {
+        width: 20%;
+
+      }
+    }
+
+    .problem_list:nth-child(2n) {
+      background-color: #F8F6F7;
+    }
+
+    .problem_list {
+      background-color: white;
+      display: flex;
+      flex-direction: row;
+      text-align: center;
+      height: 30px;
+      line-height: 30px;
+
+      .problem_time {
+        width: 30%;
+      }
+
+      .problem_type {
+        width: 25%;
+      }
+
+      .problem_status {
+        width: 25%;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        .status_txt {
+          color: white;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border-radius: 5px;
+          width: 70%;
+          height: 80%;
+          font-size: 15px !important;
+        }
+
+        .status_ok {
+          background-color: $common_success;
+        }
+
+        .status_nok {
+          background-color: $common_fail;
+        }
+
+        .status_appoint {
+          background-color: $common_warning;
+        }
+      }
+
+      .problem_opera {
+        width: 20%;
+        color: $common_blue;
+
+      }
+    }
+
+    .dialogBox {
+      width: 95vw;
+      max-height: 80vh;
+      border-radius: 5px;
+      overflow: scroll;
+
+      .cellBox {
+        display: flex;
+        justify-content: space-between;
+        font-size: 16px;
+
+        .cell_label {
+          color: $common_gray;
+        }
+
+        .cell_value {
+          font-weight: 600;
+        }
+      }
+    }
+  }
+
+
+</style>
--- src/views/problems/myProblems.vue
+++ src/views/problems/myProblems.vue
@@ -9,88 +9,22 @@
       <van-tabs class="sel_tab" v-model="active" animated swipeable>
         <tab :title="'发布工单'">
           <div class="form">
-            <form1 :mh="'72vh'">
-              <template v-slot:title>
-                发布工单
-              </template>
-              <template v-slot:list-title>
-                <div class="listTitle">
-                  <div class="title_time">创建者</div>
-                  <div class="title_desc">工单id</div>
-                  <div class="title_status">工单状态</div>
-                  <div class="title_type">操作</div>
-                </div>
-              </template>
-              <template v-slot:list>
-                <div class="problem_list" v-show="problems&&problems.length"
-                     v-for="pro,index in problems" :key="index">
-                  <div class="problem_time van-ellipsis">{{pro.createUser}}</div>
-                  <div class="problem_desc">{{pro.id}}</div>
-                  <div class="problem_status">
-                    <div class="status_txt"
-                         :class="{status_ok:pro.orderState==myConst.GD_STATE.FINISH,status_nok:pro.orderState==myConst.GD_STATE.NOT,
-            status_appoint:pro.orderState==myConst.GD_STATE.DOING,}"
-                    >
-                      {{showState(pro.orderState)}}
-                    </div>
-                  </div>
-                  <div @click="clickPro(pro)" class="problem_type">查看</div>
-
-                </div>
-              </template>
-              <template v-slot:footer>
-<!--                <van-pagination @change="getList" v-show="total>0" :total-items="total" v-model="queryParams.pageIndex"-->
-<!--                                :items-per-page="queryParams.pageSize"/>-->
-              </template>
-
-            </form1>
+            <problemForm :title="'发布工单'" :gongdanList="publishList"></problemForm>
           </div>
         </tab>
-        <tab :title="'处理工单'">
+        <tab :title="'已处理工单'">
           <div class="form">
-            <form1 :mh="'72vh'">
-              <template v-slot:title>
-                处理工单
-              </template>
-              <template v-slot:list-title>
-                <div class="listTitle">
-                  <div class="title_time">创建者</div>
-                  <div class="title_desc">工单id</div>
-                  <div class="title_status">工单状态</div>
-                  <div class="title_type">操作</div>
-                </div>
-              </template>
-              <template v-slot:list>
-                <div class="problem_list" v-show="handleProblems&&handleProblems.length"
-                     v-for="pro,index in handleProblems" :key="index">
-                  <div class="problem_time van-ellipsis">{{pro.createUser}}</div>
-                  <div class="problem_desc">{{pro.id}}</div>
-                  <div class="problem_status">
-                     <div class="status_txt"
-                         :class="{status_ok:pro.orderState==myConst.GD_STATE.FINISH,status_nok:pro.orderState==myConst.GD_STATE.NOT,
-            status_appoint:pro.orderState==myConst.GD_STATE.DOING,}"
-                    >
-                      {{showState(pro.orderState)}}
-                    </div>
-<!--                    <div class="status_txt"-->
-<!--                         :class="{status_ok:pro.nodeState==myConst.GD_NODE_STATE.FINISH,-->
-<!--                    status_nok:(pro.nodeState==myConst.GD_NODE_STATE.NOT||pro.nodeState==myConst.GD_NODE_STATE.BACK||pro.nodeState==myConst.GD_NODE_STATE.REFUSE),-->
-<!--            status_appoint:pro.nodeState==myConst.GD_NODE_STATE.WAITING,}"-->
-<!--                    >-->
-<!--                      {{showNodeState(pro.nodeState)}}-->
-<!--                    </div>-->
-
-                  </div>
-                  <div @click="clickMore(pro)" class="problem_type">查看</div>
-
-                </div>
-              </template>
-              <template v-slot:footer>
-<!--                <van-pagination @change="getList" v-show="total>0" :total-items="total" v-model="queryParams.pageIndex"-->
-<!--                                :items-per-page="queryParams.pageSize"/>-->
-              </template>
-
-            </form1>
+            <problemForm :title="'已处理工单'" :gongdanList="finishList"></problemForm>
+          </div>
+        </tab>
+        <tab :title="'未处理工单'">
+          <div class="form">
+            <problemForm :title="'未处理工单'" :gongdanList="notFinishList"></problemForm>
+          </div>
+        </tab>
+        <tab :title="'回退工单'">
+          <div class="form">
+            <problemForm :title="'回退工单'" :gongdanList="setBackList"></problemForm>
           </div>
         </tab>
       </van-tabs>
@@ -100,6 +34,7 @@
 </template>
 
 <script>
+  import problemForm from '@/components/gongdi/gongdan/problemForm'
   import util from '@/views/problems/common'
   import { ImagePreview } from 'vant'
   import navBar from '@/components/navBar'
@@ -110,6 +45,7 @@
   export default {
     name: 'allProblems',
     components: {
+      problemForm,
       navBar,
       form1,
       tab
@@ -117,7 +53,11 @@
     watch: {},
     data() {
       return {
-        active:null,
+        publishList: [],
+        finishList: [],
+        notFinishList: [],
+        setBackList: [],
+        active: null,
         gdList: [],
         problems: [],
         handleProblems: [],
@@ -138,26 +78,26 @@
     methods: {
       clickMore(row) {
         for (let i = 0; i < this.gdList.length; i++) {
-            let item = this.gdList[i]
-            if (item.orderInfo.id == row.id) {
-              this.$store.state.orderData  = item;
-              break
-            }
+          let item = this.gdList[i]
+          if (item.orderInfo.id == row.id) {
+            this.$store.state.orderData = item
+            break
           }
+        }
         this.$router.push({
-          name: 'myProblemsList',
+          name: 'myProblemsList'
         })
       },
       clickPro(row) {
         for (let i = 0; i < this.gdList.length; i++) {
           let item = this.gdList[i]
           if (item.orderInfo.id == row.id) {
-            this.$store.state.orderData  = item;
+            this.$store.state.orderData = item
             break
           }
         }
         this.$router.push({
-          name: 'problemTotalView',
+          name: 'problemTotalView'
         })
       },
       clickCheck() {
@@ -168,46 +108,6 @@
       },
       showNodeState(nodeState) {
         return this.myConst.GD_NODE_STATE_LABEL[nodeState]
-      },
-      combineFormFile(data){
-        let orders = data.orders;
-        for (let i = 0; i < orders.length; i++) {
-          let imgs = orders[i].imgs;
-          if(imgs&&imgs.length){
-            let imgsObj = {};
-            for(let i=0;i<imgs.length;i++){
-              let ii = imgs[i];
-              let fieldName =ii.fieldName;
-              let thumbnail = ii.thumbnail;
-              let img = ii.img;
-              let imgUrl = `${this.myConst.gdIp}/profile/${img}`
-              if(!imgsObj[fieldName]){
-                imgsObj[fieldName]=[];
-              }
-              // imgsObj[fieldName].push(imgUrl)
-              imgsObj[fieldName].push({ url: imgUrl})
-            }
-            let works =  orders[i].works;
-            for(let j=0;j<works.length;j++){
-              let work = works[j];
-              if(work.workData){
-                let workData = JSON.parse(work.workData)
-                let formDesc = workData.formDesc;
-                let formData = workData.formData;
-                let imgsObjKeys = Object.keys(imgsObj);
-                let formDescKeys = Object.keys(formDesc);
-                for(let k=0;k<imgsObjKeys.length;k++){
-                  let key =imgsObjKeys[k]
-                  if(formDescKeys.indexOf(key)!=-1){
-                    formData[key] = imgsObj[key]
-                  }
-                }
-                work.workData = JSON.stringify(workData);
-
-              }
-            }
-          }
-        }
       },
       getList() {
         let req = {}
@@ -221,10 +121,11 @@
         let name = this.$store.state.constructionSite[id].name
         req.orderStyle = name
         this.$gdApi.getOrderInfoByUser(req).then(res => {
+          this.loading = false
           if (res.code == SUCCESS) {
-            let orders =  res.data.orders
-            this.$store.state.gdList = orders;
-            this.gdList = orders;
+            let orders = res.data.orders
+            this.$store.state.gdList = orders
+            this.gdList = orders
             res.data.orders.sort(function(a, b) {
               return b.orderInfo.createTime - a.orderInfo.createTime
             })
@@ -234,40 +135,56 @@
             res.data.orders.forEach((item) => {
               item.orderInfo.dealTime = new Date(item.orderInfo.dealTime).toLocaleDateString().split(' ')[0]
             })
-            for(let i=0;i<orders.length;i++){
-              let order = orders[i];
-              let imgs = order.imgs;
-              let works = order.works;
-              util.combineImgField(imgs,works);
-            }
+            this.publishList = []
+            this.finishList = []
+            this.notFinishList = []
+            this.setBackList = []
             let account = this.$store.state.account
-            this.handleProblems = []
-            this.problems = []
-            for (let i = 0; i < res.data.orders.length; i++) {
-              let item = res.data.orders[i]
-              let createUser = item.orderInfo.createUser
-              if (createUser == account) {
-                this.problems.push(item.orderInfo)
-              }
-            }
-            for (let i = 0; i < res.data.orders.length; i++) {
-              let item = res.data.orders[i]
-              let works =  item.works
-              for(let j=0;j<works.length;j++){
+            for (let i = 0; i < orders.length; i++) {
+              let order = orders[i]
+              let imgs = order.imgs
+              let works = order.works
+              util.combineImgField(imgs, works)
+              for (let j = 0; j < works.length; j++) {
                 let work = works[j]
-                if(work.nodeType!='开始'&&work.dealUser==account){
-                  this.handleProblems.push(item.orderInfo)
-                  break
+                if (work.dealUser == account) {
+                  let nodeType = work.nodeType
+                  let nodeState = work.nodeState
+                  switch (nodeState) {
+                    case this.myConst.GD_NODE_STATE.NOT:
+                      this.notFinishList.push({ order, work })
+                      break
+                    case this.myConst.GD_NODE_STATE.FINISH:
+                      if (nodeType == '开始') {
+                        this.publishList.push({ order, work })
+                      } else {
+                        this.finishList.push({ order, work })
+                      }
+                      break
+                    case this.myConst.GD_NODE_STATE.BACK:
+                      this.setBackList.push({ order, work })
+                      break
+                    case this.myConst.GD_NODE_STATE.WAITING:
+                      this.finishList.push({ order, work })
+                      break
+                    case this.myConst.GD_NODE_STATE.REFUSE:
+                      this.finishList.push({ order, work })
+                      break
+                    case this.myConst.GD_NODE_STATE.ACCEPT:
+                      this.finishList.push({ order, work })
+                      break
+                  }
                 }
               }
             }
-            this.total = res.data.pageSize * this.queryParams.pageSize
+            // console.log(this.publishList, this.finishList, this.notFinishList)
           } else {
-            this.problems = []
-            this.handleProblems = []
+            this.publishList = []
+            this.finishList = []
+            this.notFinishList = []
+            this.setBackList = []
 
           }
-          this.loading = false
         })
 
       },
@@ -304,95 +221,6 @@
         overflow: scroll;
 
         .form {
-          /*background-color: white;*/
-          border-radius: 10px;
-
-
-          .listTitle {
-            display: flex;
-            flex-direction: row;
-            text-align: center;
-            height: 30px;
-            line-height: 30px;
-
-            .title_time {
-              width: 20%;
-            }
-
-            .title_desc {
-              width: 45%;
-            }
-            .title_status {
-              width: 20%;
-
-            }
-            .title_type {
-              width: 15%;
-
-            }
-
-
-          }
-
-          .problem_list:nth-child(2n) {
-            background-color: #F8F6F7;
-          }
-
-          .problem_list {
-            background-color: white;
-            display: flex;
-            flex-direction: row;
-            text-align: center;
-            height: 30px;
-            line-height: 30px;
-
-            .problem_time {
-              width: 20%;
-            }
-
-            .problem_desc {
-              width: 45%;
-            }
-
-
-
-            .problem_status {
-              width: 20%;
-              display: flex;
-              justify-content: center;
-              align-items: center;
-
-              .status_txt {
-                color: white;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 5px;
-                width: 70%;
-                height: 80%;
-                font-size: 15px !important;
-              }
-
-              .status_ok {
-                background-color: $common_success;
-              }
-
-              .status_nok {
-                background-color: $common_fail;
-              }
-
-              .status_appoint {
-                background-color: $common_warning;
-              }
-            }
-            .problem_type {
-              width: 15%;
-              color: $common_blue;
-
-            }
-          }
-
-
         }
 
       }
